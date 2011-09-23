533308f0a68773857f185eaf34ac8c432fba9063
Eli Barzilay <eli@barzilay.org>
2011-09-23 08:38:13 -0400

    Adapt existing tests to use `test'.
    This is more verbose than the previous version, but that's because
    `test' is more general.  Specifically, it throws an error with the
    actual test *syntax* that was used -- for example,
    
      (test (+ 1 2) => 4)
    
    works as expected, but if we do this with a testing utility:
    
      (define (test-add x y z)
        (test (+ x y) => z))
      (test-add 1 2 4)
    
    then the error will show the actual expression:
    
      (+ x y): expected 4, got 3
    
    and we don't see the actual values that were used.

--- class/env-functional-interpreter-compiler/analyzer-tests.rkt
+++ class/env-functional-interpreter-compiler/analyzer-tests.rkt
@@ -2,126 +2,108 @@
 
 (require "analyzer-core.rkt" "../asp.rkt")
 
-(define test
-  (lambda (test-lst)
-    (apply-test-list derive-analyze-eval test-lst)))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Primitive procedures tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (primitive-tests)
-  (let ((test-out (list '((* 3 4) 12)
-                        '((+ 3 4) 7)
-                        '((- 3 4) -1)
-                        '((/ 4 2) 2)
-                        '((null? ()) #t)
-                        '((> 3 4) #f)
-                        '((< 3 4) #t)
-                        '((= 3 4) #f)
-                        '((car (list 3 4)) 3)
-                        '((cdr (list 3 4)) (4))
-                        '((cons 3 (cons 4 ())) (3 4))
-                        )))
-    (test test-out)))
+  (test (derive-analyze-eval '(* 3 4)) => 12)
+  (test (derive-analyze-eval '(+ 3 4)) => 7)
+  (test (derive-analyze-eval '(- 3 4)) => -1)
+  (test (derive-analyze-eval '(/ 4 2)) => 2)
+  (test (derive-analyze-eval '(null? ())) => #t)
+  (test (derive-analyze-eval '(> 3 4)) => #f)
+  (test (derive-analyze-eval '(< 3 4)) => #t)
+  (test (derive-analyze-eval '(= 3 4)) => #f)
+  (test (derive-analyze-eval '(car (list 3 4))) => 3)
+  (test (derive-analyze-eval '(cdr (list 3 4))) => '(4))
+  (test (derive-analyze-eval '(cons 3 (cons 4 ()))) => '(3 4)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Application and `lambda' tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (app-lambda-tests)
-  (let ((test-out (list '(((lambda (x) x) 12) 12)
-                        '(((lambda (x y z) (+ x y z)) 12 13 14) 39)
-                        '(((lambda (x) ((lambda (x) (+ x 1)) 2)) 12) 3)
-                        '(((lambda (f x y) (f x y)) + 12 4) 16)
-                        '(((lambda (f x y) (f x y)) ((lambda () +)) 12 4) 16)
-                        )))
-    (test test-out)))
+  (test (derive-analyze-eval '((lambda (x) x) 12)) => 12)
+  (test (derive-analyze-eval '((lambda (x y z) (+ x y z)) 12 13 14)) => 39)
+  (test (derive-analyze-eval '((lambda (x) ((lambda (x) (+ x 1)) 2)) 12)) => 3)
+  (test (derive-analyze-eval '((lambda (f x y) (f x y)) + 12 4)) => 16)
+  (test (derive-analyze-eval '((lambda (f x y) (f x y)) ((lambda () +)) 12 4)) => 16))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; `begin' tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (begin-tests)
-  (let ((test-out (list '((begin 1 2 3) 3)
-                        '((begin 1 2 ((lambda () 5))) 5)
-                        )))
-    (test test-out)))
+  (test (derive-analyze-eval '(begin 1 2 3)) => 3)
+  (test (derive-analyze-eval '(begin 1 2 ((lambda () 5)))) => 5))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; `let' tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (let-tests)
-  (let ((test-out (list '((let ((x 1) (y 2)) (+ x y)) 3)
-                        '((let ((x 1) (y 2)) (+ x y ((lambda (x) (* 2 x)) 10))) 23)
-                        '((let ((x ((lambda () 5)))) x) 5)
-                        '((let ((f (lambda (x) (+ x x)))) (f 4)) 8)
-                        '((let ((f (lambda (x) (+ x x))) (g (lambda (x y) (* x y))) (h (lambda () 2))) (f (g 3 (h)))) 12)
-                        )))
-    (test test-out)))
+  (test (derive-analyze-eval '(let ((x 1) (y 2)) (+ x y))) => 3)
+  (test (derive-analyze-eval '(let ((x 1) (y 2)) (+ x y ((lambda (x) (* 2 x)) 10)))) => 23)
+  (test (derive-analyze-eval '(let ((x ((lambda () 5)))) x)) => 5)
+  (test (derive-analyze-eval '(let ((f (lambda (x) (+ x x)))) (f 4))) => 8)
+  (test (derive-analyze-eval '(let ((f (lambda (x) (+ x x))) (g (lambda (x y) (* x y))) (h (lambda () 2))) (f (g 3 (h))))) => 12))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; definition and function-definition tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (definition-tests)
-  (let ((test-out (list '((define x 2) ok)
-                        '((define (f x) (+ x x)) ok)
-                        '(x 2)
-                        '((f x) 4)
-                        )))
-    (test test-out)))
+  (test (derive-analyze-eval '(define x 2)) => 'ok)
+  (test (derive-analyze-eval '(define (f x) (+ x x))) => 'ok)
+  (test (derive-analyze-eval 'x) => 2)
+  (test (derive-analyze-eval '(f x)) => 4))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; `if' tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (if-tests)
-  (let ((test-out (list '((if (> 3 2) 5 1) 5)
-                        '((if (< 3 2) 5 1) 1)
-                        '((if (> (+ 1 2) 2) (+ 1 2 3) 1) 6)
-                        '((define (f n) (if (= n 0) 1 (* n (f (- n 1))))) ok)
-                        '((f 5) 120)
-                        )))
-    (test test-out)))
+  (test (derive-analyze-eval '(if (> 3 2) 5 1)) => 5)
+  (test (derive-analyze-eval '(if (< 3 2) 5 1)) => 1)
+  (test (derive-analyze-eval '(if (> (+ 1 2) 2) (+ 1 2 3) 1)) => 6)
+  (test (derive-analyze-eval '(define (f n) (if (= n 0) 1 (* n (f (- n 1)))))) => 'ok)
+  (test (derive-analyze-eval '(f 5)) => 120))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; `cond' tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (cond-tests)
-  (let ((test-out (list '((cond ((> 3 2) 5) (else 1)) 5)
-                        '((cond ((< 3 2) 5) (else 1)) 1)
-                        '((let ((x 3)) (cond ((= x 1) 10) ((= x 2) 20) ((= x 3) 30) (else 100))) 30)
-                        '((let ((x 10)) (cond ((= x 1) 10) ((= x 2) 20) ((= x 3) 30) (else 100))) 100)
-                        )))
-    (test test-out)))
+  (test (derive-analyze-eval '(cond ((> 3 2) 5) (else 1))) => 5)
+  (test (derive-analyze-eval '(cond ((< 3 2) 5) (else 1))) => 1)
+  (test (derive-analyze-eval '(let ((x 3)) (cond ((= x 1) 10) ((= x 2) 20) ((= x 3) 30) (else 100)))) => 30)
+  (test (derive-analyze-eval '(let ((x 10)) (cond ((= x 1) 10) ((= x 2) 20) ((= x 3) 30) (else 100)))) => 100))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Y-combinator tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (y-comb-tests)
-  (let ((test-out (list '((( (lambda (f) (f f))
-                             (lambda (fact)
-                               (lambda (n)
-                                 (if (= n 0)
-                                     1
-                                     (* n ((fact fact) (- n 1)))))))
-                           6) 720)
-                        )))
-    (test test-out)))
+  (test (derive-analyze-eval '(( (lambda (f) (f f))
+                                 (lambda (fact)
+                                   (lambda (n)
+                                     (if (= n 0)
+                                         1
+                                         (* n ((fact fact) (- n 1)))))))
+                               6))
+        => 720))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Invoking tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(primitive-tests)
-(app-lambda-tests)
-(begin-tests)
-(let-tests)
-(definition-tests)
-(if-tests)
-(cond-tests)
-(y-comb-tests)
+(run-tests
+ (primitive-tests)
+ (app-lambda-tests)
+ (begin-tests)
+ (let-tests)
+ (definition-tests)
+ (if-tests)
+ (cond-tests)
+ (y-comb-tests))
--- class/imperative-interpreter-compiler/interpreter-tests.rkt
+++ class/imperative-interpreter-compiler/interpreter-tests.rkt
@@ -6,144 +6,125 @@
 ;;;;;;;;;;;;;;;;;;;;;;;  TESTS  ;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define test
-  (lambda (test-lst)
-    (apply-test-list derive-eval test-lst)))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Primitive procedures tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (primitive-tests)
-  (let ((test-out (list '((* 3 4) 12)
-                        '((+ 3 4) 7)
-                        '((- 3 4) -1)
-                        '((/ 4 2) 2)
-                        '((null? ()) #t)
-                        '((> 3 4) #f)
-                        '((< 3 4) #t)
-                        '((= 3 4) #f)
-                        '((car (list 3 4)) 3)
-                        '((cdr (list 3 4)) (4))
-                        '((cons 3 (cons 4 ())) (3 4))
-                        )))
-    (test test-out)))
+  (test (derive-eval '(* 3 4)) => 12)
+  (test (derive-eval '(+ 3 4)) => 7)
+  (test (derive-eval '(- 3 4)) => -1)
+  (test (derive-eval '(/ 4 2)) => 2)
+  (test (derive-eval '(null? ())) => #t)
+  (test (derive-eval '(> 3 4)) => #f)
+  (test (derive-eval '(< 3 4)) => #t)
+  (test (derive-eval '(= 3 4)) => #f)
+  (test (derive-eval '(car (list 3 4))) => 3)
+  (test (derive-eval '(cdr (list 3 4))) => '(4))
+  (test (derive-eval '(cons 3 (cons 4 ()))) => '(3 4)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Application and `lambda' tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (app-lambda-tests)
-  (let ((test-out (list '(((lambda (x) x) 12) 12)
-                        '(((lambda (x y z) (+ x y z)) 12 13 14) 39)
-                        '(((lambda (x) ((lambda (x) (+ x 1)) 2)) 12) 3)
-                        '(((lambda (f x y) (f x y)) + 12 4) 16)
-                        '(((lambda (f x y) (f x y)) ((lambda () +)) 12 4) 16)
-                        )))
-    (test test-out)))
+  (test (derive-eval '((lambda (x) x) 12)) => 12)
+  (test (derive-eval '((lambda (x y z) (+ x y z)) 12 13 14)) => 39)
+  (test (derive-eval '((lambda (x) ((lambda (x) (+ x 1)) 2)) 12)) => 3)
+  (test (derive-eval '((lambda (f x y) (f x y)) + 12 4)) => 16)
+  (test (derive-eval '((lambda (f x y) (f x y)) ((lambda () +)) 12 4)) => 16))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; `begin' tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (begin-tests)
-  (let ((test-out (list '((begin 1 2 3) 3)
-                        '((begin 1 2 ((lambda () 5))) 5)
-                        )))
-    (test test-out)))
+  (test (derive-eval '(begin 1 2 3)) => 3)
+  (test (derive-eval '(begin 1 2 ((lambda () 5)))) => 5))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; `let' tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (let-tests)
-  (let ((test-out (list '((let ((x 1) (y 2)) (+ x y)) 3)
-                        '((let ((x 1) (y 2)) (+ x y ((lambda (x) (* 2 x)) 10))) 23)
-                        '((let ((x ((lambda () 5)))) x) 5)
-                        '((let ((f (lambda (x) (+ x x)))) (f 4)) 8)
-                        '((let ((f (lambda (x) (+ x x))) (g (lambda (x y) (* x y))) (h (lambda () 2))) (f (g 3 (h)))) 12)
-                        )))
-    (test test-out)))
+  (test (derive-eval '(let ((x 1) (y 2)) (+ x y))) => 3)
+  (test (derive-eval '(let ((x 1) (y 2)) (+ x y ((lambda (x) (* 2 x)) 10)))) => 23)
+  (test (derive-eval '(let ((x ((lambda () 5)))) x)) => 5)
+  (test (derive-eval '(let ((f (lambda (x) (+ x x)))) (f 4))) => 8)
+  (test (derive-eval '(let ((f (lambda (x) (+ x x))) (g (lambda (x y) (* x y))) (h (lambda () 2))) (f (g 3 (h))))) => 12))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; definition and function-definition tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (definition-tests)
-  (let ((test-out (list '((define x 2) ok)
-                        '((define (f x) (+ x x)) ok)
-                        '(x 2)
-                        '((f x) 4)
-                        )))
-    (test test-out)))
+  (test (derive-eval '(define x 2)) => 'ok)
+  (test (derive-eval '(define (f x) (+ x x))) => 'ok)
+  (test (derive-eval 'x) => 2)
+  (test (derive-eval '(f x)) => 4))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; `if' tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (if-tests)
-  (let ((test-out (list '((if (> 3 2) 5 1) 5)
-                        '((if (< 3 2) 5 1) 1)
-                        '((if (> (+ 1 2) 2) (+ 1 2 3) 1) 6)
-                        '((define (f n) (if (= n 0) 1 (* n (f (- n 1))))) ok)
-                        '((f 5) 120)
-                        )))
-    (test test-out)))
+  (test (derive-eval '(if (> 3 2) 5 1)) => 5)
+  (test (derive-eval '(if (< 3 2) 5 1)) => 1)
+  (test (derive-eval '(if (> (+ 1 2) 2) (+ 1 2 3) 1)) => 6)
+  (test (derive-eval '(define (f n) (if (= n 0) 1 (* n (f (- n 1)))))) => 'ok)
+  (test (derive-eval '(f 5)) => 120))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; `cond' tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (cond-tests)
-  (let ((test-out (list '((cond ((> 3 2) 5) (else 1)) 5)
-                        '((cond ((< 3 2) 5) (else 1)) 1)
-                        '((let ((x 3)) (cond ((= x 1) 10) ((= x 2) 20) ((= x 3) 30) (else 100))) 30)
-                        '((let ((x 10)) (cond ((= x 1) 10) ((= x 2) 20) ((= x 3) 30) (else 100))) 100)
-                        )))
-    (test test-out)))
+  (test (derive-eval '(cond ((> 3 2) 5) (else 1))) => 5)
+  (test (derive-eval '(cond ((< 3 2) 5) (else 1))) => 1)
+  (test (derive-eval '(let ((x 3)) (cond ((= x 1) 10) ((= x 2) 20) ((= x 3) 30) (else 100)))) => 30)
+  (test (derive-eval '(let ((x 10)) (cond ((= x 1) 10) ((= x 2) 20) ((= x 3) 30) (else 100)))) => 100))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; `set!' tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (set!-tests)
-  (let ((test-out (list '((let ((x 1)) (set! x 5) x) 5)
-                        '((let ((x 1) (y 3)) (set! x (+ y x)) x) 4)
-                        '((let ((x 1)) ((lambda (x) (set! x 5)) x) x) 1)
-                        '((let ((x 1)) ((lambda (x) (set! x 5) x) x)) 5)
-                        '((let ((x 1)) ((lambda (y) (set! x (* y y))) 4) x) 16)
-                        ;; '((define f (let ((x 0)) (lambda () (set! x (+ x 1)) x))) ok)
-                        ;; '((f) 1)
-                        ;; '((f) 2)
-                        ;; '((begin (f) (f) (f) (f)) 6)
-                        )))
-    (test test-out)))
+  (test (derive-eval '(let ((x 1)) (set! x 5) x)) => 5)
+  (test (derive-eval '(let ((x 1) (y 3)) (set! x (+ y x)) x)) => 4)
+  (test (derive-eval '(let ((x 1)) ((lambda (x) (set! x 5)) x) x)) => 1)
+  (test (derive-eval '(let ((x 1)) ((lambda (x) (set! x 5) x) x))) => 5)
+  (test (derive-eval '(let ((x 1)) ((lambda (y) (set! x (* y y))) 4) x)) => 16)
+  ;; (test (derive-eval '(define f (let ((x 0)) (lambda () (set! x (+ x 1)) x)))) => 'ok)
+  ;; (test (derive-eval '(f)) => 1)
+  ;; (test (derive-eval '(f)) => 2)
+  ;; (test (derive-eval '(begin (f) (f) (f) (f))) => 6)
+  )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Y-combinator tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (y-comb-tests)
-  (let ((test-out (list '((( (lambda (f) (f f))
-                             (lambda (fact)
-                               (lambda (n)
-                                 (if (= n 0)
-                                     1
-                                     (* n ((fact fact) (- n 1)))))))
-                           6) 720)
-                        )))
-    (test test-out)))
+  (test (derive-eval '(( (lambda (f) (f f))
+                         (lambda (fact)
+                           (lambda (n)
+                             (if (= n 0)
+                                 1
+                                 (* n ((fact fact) (- n 1)))))))
+                       6))
+        => 720))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Invoking tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(primitive-tests)
-(app-lambda-tests)
-(begin-tests)
-(let-tests)
-(definition-tests)
-(if-tests)
-(cond-tests)
-(set!-tests)
-(y-comb-tests)
+(run-tests
+ (primitive-tests)
+ (app-lambda-tests)
+ (begin-tests)
+ (let-tests)
+ (definition-tests)
+ (if-tests)
+ (cond-tests)
+ (set!-tests)
+ (y-comb-tests))
